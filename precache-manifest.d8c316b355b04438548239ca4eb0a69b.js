11cce3d9202d56f43a2a52fc7e51333339c6ea6e
CodeeAndGetOld <tudorh.karp@gmail.com>
2020-04-06 13:09:36 +0300

    Updates

---
+++ precache-manifest.d8c316b355b04438548239ca4eb0a69b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "678e28d9e7c9815c14c40ef57699df07",
+    "url": "/HugbetsRegister/index.html"
+  },
+  {
+    "revision": "968a10661801b32b3ec2",
+    "url": "/HugbetsRegister/static/css/main.e683336e.chunk.css"
+  },
+  {
+    "revision": "0e4467cb63e7d4ac5efd",
+    "url": "/HugbetsRegister/static/js/2.f2804e8a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/HugbetsRegister/static/js/2.f2804e8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "968a10661801b32b3ec2",
+    "url": "/HugbetsRegister/static/js/main.9d8ed99d.chunk.js"
+  },
+  {
+    "revision": "01a0571625749ce09b13",
+    "url": "/HugbetsRegister/static/js/runtime-main.6146f5e9.js"
+  },
+  {
+    "revision": "1d6336b8aa6c86f923555ef137b92a02",
+    "url": "/HugbetsRegister/static/media/logo.1d6336b8.png"
+  }
+]);
